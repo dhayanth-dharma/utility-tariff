5050121b239cda01cbb9881d68be870a86923eaf
Dhayananth <dhayanthdharma@gmail.com>
2024-10-19 19:14:10 +0200

    Update urls.py

--- back-app/tarifftracker/urls.py
+++ back-app/tarifftracker/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
   path('api/login/', views.UserLoginView.as_view(), name='login'),
   path('api/logout/', views.UserLogoutView.as_view(), name='logout'),
   path('api/tariff/', views.tariff, name='tariff'),
+  path('api/utility_cost/', views.utility_cost_graph, name='utility_cost'),
   path('api/projects/', views.project_list, name='project_list'),
   path('api/protected/', views.ProtectedView.as_view(), name='protected'),
 ]
